662fd7136df61bd0115956e396c27e1772397b0d
gloriarustama <gloriariatama02@gmail.com>
2022-03-02 10:44:47 +0700

    Menyelesaikan Latihan di pertemuan 3

---
+++ kuliah/pertemuan3/latihan.php
@@ -0,0 +1,26 @@
+<?php 
+// Pengulangan 
+// foreach : pengulangan khusus array
+
+// for
+for( $i = 0; $i < 5; $i++ ) {
+  echo "Hello World! <br>";
+} echo '<hr>';
+
+// while
+$i = 0;
+while( $i < 5) {
+  echo "Hello World! <br>";
+$i++;
+} echo '<hr>';
+
+// do.. while
+$i = 0;
+do {
+  echo "Hello World! <br>";
+$i++;
+} while( $i < 5 );
+echo '<hr>';
+
+
+?>
---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,17 @@
+<?php 
+// Pengkondisian / Percabangan 
+// If else
+// if else if else
+// ternary
+// switch
+
+$x = 50;
+if( $x < 20 ) {
+  echo "benar";
+} else if ($x == 20) {
+  echo "bingo!";
+} else {
+  echo "salah";
+}
+
+?>
